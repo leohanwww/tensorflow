8265bb2c2e6dc2ef6cafdb6c0e5b5344bf066f34
leohanwww <pumpkinlittlehan@hotmail.com>
2019-03-19 17:23:35 +0800

    损失函数

--- tf入门.py
+++ tf入门.py
@@ -38,16 +38,16 @@ with tf.Session(graph=g2) as sess:
 
 import tensorflow as tf
 # tf.constant 是一个计算，这个计算的结果为一个张量， 保存在变量a 中。
-a= tf.constant([l . O, 2 . 0] , name =” a ”)
+a= tf.constant([1.0, 2 . 0] , name =” a ”)
 b = tf.constant( [2 . 0 , 3 . 0] , name = ” b ”)
-result = tf.add( a , b , name=” add ”)
+result = tf.add( a , b , name=” add ”)#创建add节点
 print result
 >>> Tensor( "add:0"， shape=(2,), dtype=float32)
 >>> tf.Session().run(result)#这里出结果
 array([4, 7])
 Tensorflow中的张量和Numpy里的数组有区别，TF计算的结果是一个张量结构，
 保存了三个属性：名字(name)、维度(shape)、类型(type)
-张量的命名就可以通过“ node:src_ output ”的
+张量的命名就可以通过“ node:src_output ”的
 形式来给出。其中node 为节点的名称， src一output 表示当前张量来自节点的第几个输出。
 比如上面代码打出来的"add:0"就说明了result 这个张量是计算节点“ add ” 输出的第0个结果
 
@@ -59,11 +59,13 @@ sess.run(...)
 sess.close()
 使用上下文管理器
 with tf.Session() as sess:
-	sess.run()
+	sess.run(result)
+array([2., 4.], dtype=float32)
 
 sess = tf.Session()#指定sess为默认会话
 with sess.as_default():
 	print (result.eval())
+[2. 4.]
 
 交互式环境里可以使用：
 >>>sess = tf.InteractiveSession()#通过设置默认会话的方式来获取张量的取值
@@ -72,83 +74,131 @@ with sess.as_default():
 通过tf.InteractiveSession 函数可以省去将产生的会话注册为默认会话的过程
 
 config = tf.ConfigProto(allow soft placement=True,log_device_placement=True)
+#设置运行session的参数
 #注册为默认会话，第一个参数为True可以自动调整运行在cpu或gpu上
 sessl = tf.InteractiveSession(config=config)
 sess2 = tf.Session(config=co nfig)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+前向传播算法
+之所以称之为全连接神经网络是因为相邻两层之间任意两个节点之间都有连接
+矩阵乘法实现，x是输入，w1，w2是参数
+a = tf.matmul(x,w1) #相当于numpy里的dot操作
+y = tf.matmul(a,w2)
+
+weights = tf.Variable(tf.random_normal([2,3], stddec=2))#生成随机权重
+随机生成函数
+tf.random_normal	正态分布	参数：平均值，标准差，类型
+tf.truncated_normal		正态分布，如果随机值偏离平均值2个标准差，重新随机
+tf.random_uniform	均匀分布
+tf.random_gamma
+
+tf.zeros	tf.zeros([2, 3), int32) > ((0, 0, O] , [O , 0, O]]
+tf.ones	tf.ones([2, 3] , int32) -> [[1 , 1 , 1) , [1 , 1 , 1))
+tf.fill	tf.fill([2, 3), 9) -> ((9, 9, 9) , (9 , 9, 9))
+tf.constant常数	tf.constant((1 , 2, 3)) -> (1 ,2,3)
+
+b = tf.Variable(tf.zeros([3]))
+w2 = tf.Variable(weights.initialized_value() * 2.0)
+
+import tensorf low as tf
+＃声明wl 、w2 两个变盘。这里还通过seed 参数设定了随机种子，
+＃这样可以保证每次运行得到的结果是一样的。
+wl = tf.Variable(tf.random_normal((2, 3) , stddev=l , seed=l))
+w2 = tf.Variable(tf .random_normal((3, 1), stddev=l, seed=l))
+#暂时将输入的特征向盘定义为一个常量。注意这里x 是一个lx2 的矩阵。
+x = tf.constant([[0.7, 0.9)))
+＃通过3.4.2 节描述的前向传播算法获得神经网络的输出。
+a = tf.matmul (x, wl)
+y = tf.matmul(a, w2)
+sess = tf.Session()
+＃与3.4.2 中的计算不同，这里不能直接通过sess.run(y）来获取y的取值，
+＃因为wl 和w2 都还没有运行初始化过程。以下两行分别初始化了wl 和w2 两个变量。
+sess.run(wl.initializer) ＃初始化wl.
+sess.run(w2.initializer) ＃初始化w2 。
+＃输出［（ 3.95757794 ））。
+print (sess.run(y))
+sess.close()
+#初始化所有变量
+init_op = tf.global_variables_initializer()
+sess.run(init_op)
+
+参数的数据类型不能改变
+参数的维度通过设置validata_shape=False
+tf.assign(wl , w2 , validate shape=False)
+
+运用placeholder动态提供输入数据
+
+import tensorf low as tf
+wl = tf.Variable(tf.random normal([2 , 3], stddev=l ))
+w2 = tf.Variable(tf.random normal([3, 1], stddev=l))
+＃定义placeholder 作为存放输入数据的地方。这里维度也不一定要定义。
+＃但如果维度是确定的，那么给出维度可以降低出错的概率。
+x = tf.placeholder(tf.float32,shape=(1,2),name="input")
+a = tf.matmul(x,w1)
+y = tf.matmul(a,w2)
+
+sess = tf.Session()
+init_op = tf.global_variables_initializer()
+sess.run(init_op)
+print(sess.run(y , feed_ dict={x: [[0.7,0.9]]}))#输入x的数值
+batch输入同样，只要扩大x的维度即可
+
+反向传播
+＃使用sigmoid 函数将y 转换为0 ～ 1 之间的数值。转换后y 代表预测是正样本的概率， 1-y 代表
+＃预测是负样本的概率。
+y=tf.sigmoid(y )
+＃定义损失函数来刻画预测值与真实值得差距。
+cross_entropy = ’ tf.reduce_ mean(
+y_ * tf.l 。g(tf.clip_by_value(y , le-10, 1.0))
++(1-y)*tf.log(tf.clip_by_value (l-y , le-10, 1 .0)))
+＃定义学习率，在第4 章中将更加具体的介绍学习率。
+learning_rate = 0.001
+＃定义反向传播算法来优化神经网络中的参数。
+train step =\
+tf.train.AdamOptimizer(learning_rate) .minimize (cross_entropy)
+
+
+完整的神经网络
+import tensorflow as tf
+from numpy.random import RandomState
+
+batch_size = 8
+
+w1 = tf.Variable(tf.random_normal([2,3],stddev=1,seed=1))
+w2 = tf.Variable(tf.random_normal([3,1],stddev=1,seed=1))
+
+x = tf.palceholder(tf.float32,shape=(None,2),name='x-input')
+y_ = tf.placeholder(tf.float32,shape=(None,2),name='y-input')
+
+a = tf.matmul(x,w1)
+y = tf.matmul(a,w2)
+
+y = tf.sigmoid(y)#使用sigmoid 函数将y 转换为0 ～ 1 之间的数值。转换后y 代表预测是正样本的概率
+learningrate = 0.001
+
+cross entropy= -tf.reduce mean(
+	y * tf.log(tf.clip by value(y , le- 10 , 1.0))
+	+(1-y)*tf.log(tf.clip_by_value ( l - y , le - 10 , 1.0 )))
+train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+
+#通过随机数生成一个模拟数据集。
+rdm = RandomState(l)
+dataset_size = 128
+X = rdm.rand(dataset size , 2 )
+Y = [ [int(xl+x2 < 1)) for (xl , x2 ) in X)
+
+with tf.Session() as sess:
+	init_op = tf.global_variables_initializer()
+	sess.run(init_op)#初始化参数
+
+STEPS = 5000
+for i in range(STEPS):
+	start = ( i * batch size ） 告dataset size
+	end = min (start+batch size , dataset size )
+
+sess.run(train_step,feed_dict={x : X[start : end), y : Y[start : end ) })
+if i % 1000 == 0 
+	total_cross_entropy = sess.run(cross entropy , feed dict={x : X, y : Y})
 
 
 
---
+++ 深层神经网络.py
@@ -0,0 +1,136 @@
+深层神经网络
+线性模型只能做简单的区分问题，复杂问题无法通过直线划分
+
+使用激活函数使得输出不再是线性的
+tf.nn.relu
+tf.sigmoid
+tf.tanh
+
+a = tf.nn.relu(tf.matmul(x,w1)+b)
+y = tf.nn.relu(tf.matmul(a,w2)+b)
+
+增加层以解决异或问题
+单层神经网络无法解决异或问题（相同符号为0，不同符号为1）
+深层神经网络实际上有组合特征提取的功能。这个特性对于解决不易提取特征向量的问题（比如图片识别、语音识别等）有很大帮助，通过添加一层神经网络，这层网络识别出了分界线
+
+损失函数
+分类问题的输出采用softmax层，用原神经网络的输出作为softmax的输入，把最终输出变成概率分布，从而可以使用交叉嫡来计算预测概率分布和答案之间的距离（交叉嫡就是计算两个概率分布之间距离的函数）
+
+cross_entropy = -tf.reduce_mean(y_ * tf.log(tf.clip_by_value(y,1e-10, 1.0)))
+y_表示正确结果
+y表示预测结果
+clip_by_value是一个标准化函数，规定了下限和上限
+v = tf.constant([[1.0, 2.0 , 3 .0], [4.0 , 5.0,6.0]))
+print tf.clip_by_value(v, 2 .5, 4 . 5) . eval()
+#输出［［ 2 .5 2 . 5 3.] [ 4. 4 . 5 4.5]]#小于2.5的取2.5，大于4.5的取4.5
+tf.reduce_mean求矩阵的平均数
+可以直接使用tf里的函数
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(lmabels=y_, logits=y)
+
+回归问题用于预测一个数值
+mse = tf.reduce_mean(tf.square(y_ - y))#均方误差损失函数
+
+自订损失函数
+
+import tensorf low as tf
+from numpy . random import RandomState
+batch_size = 8
+
+x = tf.placeholder(tf.float32, shape=(None,2),name='x-input')
+y_ = tf.placeholder(tf.float32, shape=(None,2), name='y-input')
+
+w1 = tf.Variable(tf.random_normal([2,1],stddv=1,seed=1))
+y = tf.matmul(x,w1)
+
+loss_less = 10
+loss_more = 1
+loss = tf.reduce_sum(tf.whers(tf.greater(y,y_),(y - y_)*loss_more,(y_ - y)*loss_less))
+train_step= tf.train.AdamOptimizer(0.001).minimize(loss)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
