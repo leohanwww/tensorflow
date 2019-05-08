70bfa336e1c5432303bda913090b40e474816f73
leohanwww <leohanwww@gmail.com>
2019-05-08 09:37:11 +0800

    整理图像数据流为tf输入

--- 图像数据处理.py
+++ 图像数据处理.py
@@ -79,7 +79,7 @@ pixels = tf.cast(features[’pixels’], tf.int32}
 sess = tf.Session()
 #启动多线程处理输入数据
 coord = tf.train.Coordinator(}
-threads= tf.train.start_queue_runners(sess=sess, coord=coord}
+threads = tf.train.start_queue_runners(sess=sess, coord=coord}
 #每次运行可以读取TFRecord 文件中的一个样例。当所有样例读完之后，在此样例中程序
 #会再从头读取。
 for i in range(10} :
@@ -194,13 +194,6 @@ with tf.Session() as tf:
         v,_ = sess.run([x,q_inc])
         print v
 
-import tensorflow as tf
-#使用tf.QueueRunner启动多个线程操作同一个队列
-queue = tf.FIFIQueue(100,'float')
-enqueue_op = queue.enqueue([tf.random_normal([1])])#定义队列入队操作
-qr = tf.train.QueueRunner(queue,[enqueue_op] * 5)#表示启动5个线程，每个线程进行enqueue_op操作
-tf.train.add_queue_runner(qr)#第二个参数不写则将qr加入默认集合tf.GraphKeys.QUEUE_RUNNERS
-out_tensor = queue.dequeue()#定义出队操作
 
 多线程操作
 coord = tf.train.Coordinator()#创建一个实例来协同多线程
@@ -237,6 +230,287 @@ with tf.Session() as sess:
 	for _ in range(3): print(sess.run(out_tensor)[0])
 	coord.request_stop()
 	coord.join(threads)
+
+
+输入文件队列
+
+num_shards = 2#总文件数
+instances_per_shard = 2#每个文件多少数据
+#把输入转换成TFRecord
+for i in range(num_shards):
+	filename = ('/path/data.tfrecords-%.5d-of-%.5d' % (i, num_shards))
+	writer = tf.python_io.TFRecordWriter(filename)
+	for j in range(instances_per_shard):#将数据封装成Example结构并写入TFRcecord文件
+		example = tf.train.Example(features=tf.train.Features(feature={
+		'i': _int64_feature(i),
+		'j': _int64_feature(j)
+		}
+		))
+	writer.write(example.SerializerToString())
+writer.close()
+
+读取执行
+files = tf.train.match_filenames_once('/path/data.tfrecords-*')
+filename_queue = tf.train.string_input_producer(files, shuffle=False)#创建输入队列
+reader = tf.TFRecordReader()
+_, serialized_example= reader.read(filename_queue)
+features = tf.parse_single_example(
+	serialized_example,
+	features = {
+		'i': tf.FixedLenFeature([], tf.int64),
+		'j': tf.FixedLenFeature([], tf.int64),
+		})
+		
+with tf.Session() as sess:
+	tf.local_variables_initializer().run()
+	print(sess.run(files))
+	
+	coord = tf.train.Coordinator()
+	threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+	
+	for i in range(6):
+		print(sess.run([features['i'], features['j']]))
+	coord.request_stop()
+	coord.join(threads)
+	
+
+组合训练数据
+batch_size = 3
+example,label = features[i], features[j]
+capacity = 1000 + 3 * batch_size
+
+example_batch, label_batch = tf.train.batch([example, label], batch_size=batch_size,
+	capacity=capacity)
+	#此函数将[example, label]整理成输入batch队列,自动管理队列出入
+with tf.Session() as sess:
+	tf.initializer_all_variables.run()
+	coord = tf.train.Coordinator()
+	threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+	
+	for i in range(2):
+		cur_exapmle_batch, cur_label_batch = sess.run(
+			[example_batch, label_batch])
+		print(cur_exapmle_batch, cur_label_batch)
+	
+	coord.request_stop()
+	coord.join(threads)
+
+
+
+完整数据处理框架
+
+import tensorflow as tf
+
+files = tf.train.match_filenames_once('path/file_pattern-*')
+filename_queue = tf.train.string_input_producer(files, shuffle=False)
+#产生输入队列的函数
+
+reader = tf.TFRecordReader()
+_, serialized_example = reader.read(filename_queue)#读取序列
+features = tf.parse_single_example(serialized_example,
+	features = {
+	'image': tf.FixedLenFeature([], tf.string),
+	'label': tf.FixedLenFeature([], tf.int64),
+	'height': tf.FixedLenFeature([], tf.int64),
+	'weight': tf.FixedLenFeature([], tf.int64),
+	'channels': tf.FixedLenFeature([], tf.int64),
+	}
+	)
+image = features['image']
+label = features['label']
+height = features['height']
+weight = features['weight']
+channels = features['channels']
+
+decode_image = tf.decode_raw(image, tf.uint8)#解码tensor
+decode_image.set_shape([height,weight,channels])
+#前面的预处理图片函数
+image_size = 299
+distort_image = preprocess_for_train(decode_image, image_size, image_size, None)
+
+#整理成输入batch队列
+min_after_dequeue = 10000
+batch_size = 100
+capacity = min_after_dequeue + 3 * batch_size
+image_batch, label_batch = tf.train.shuffle_batch(
+	[distorted_image, label], batch_size=batch_size,
+	capacity=capacity, min_after_dequeue=min_after_dequeue
+	)
+
+logit = inference(image_batch)
+loss = calc_loss(logit, label_batch)
+train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+
+with tf.Session() as sess:
+	tf.global_variables_initializer().run()
+	tf.local_variables_initializer().run()
+	coord = tf.Coordinator()
+	threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+	for i in(training_step):
+		sess.run(train_step)
+		
+	coord.request_stop()
+	coord.join()
+
+
+数据集操作
+
+input_data = [1, 2, 3, 5, 8]
+dataset = tf.data.Dataset.from_tensor_slice(input_data)
+iterator = dataset.make_one_shot_iterator()
+x = iterator.get_next()
+
+dataset = tf.data.TextLineDataset(input_files)#从文本构建
+
+#使用TFRecord需要定义parsr
+def parser(record):
+	features = tf.parse_single_example(
+		record,
+		features={
+			'f1': tf.FixedLenFeature([],tf.int64)
+			'f2': tf.FixedLenFeature([],tf.int64)
+			})
+	return features['f1'], features['f2']
+
+input_files = '/path/to/TFRecordfile'
+dataset = tf.data.TFRecordDataset(input_files)
+dataset = dataset.map(parser)#对每条数据调用parser方法
+iterator = dataset.make_one_shot_iterator()
+f1, f2 = iterator.get_next()
+with tf.Session as sess:
+	for i in range(10):
+		print(sess.run[f1, f2])
+
+#使用placeholder需要初始化
+iterator = dataset.make_initializer_iterator()
+#从TFRecord文件创建数据集,具体文件路径是palceholder
+f1, f2 = iterator.get_next()
+with tf.Session() as sess:#需要初始化
+	sess.run(iterator.initializer,
+			feed_dict={input_files:['/path/to/TFRecordfile']})
+	while True:
+		try:
+			sess.run([f1, f2])
+		except tf.errors.OutOfRangeError:
+			break
+
+
+
+处理输入数据集
+
+import tensorflow as tf
+
+train_files = tf.train.match_filenames_once('/path')#这里读取TFRecord文件
+test_files = tf.train.match_filenames_once('/path')
+
+def parser(record):
+	features = tf.parse_single_example(
+		record,
+		features={
+			'image': tf.FixedLenFeature([], tf.string),
+			'label': tf.FixedLenFeature([], tf.int64),
+			'height': tf.FixedLenFeature([], tf.int64),
+			'weight': tf.FixedLenFeature([], tf.int64),
+			'channels': tf.FixedLenFeature([], tf.int64),
+			}
+	)
+
+	decode_image = tf.decode_raw(features['image'], tf.uint8)
+	decode_image.set_shape([features['height'], features['weight'], features['channels']])
+	label = features['label']
+	return decode_image, label
+	
+image_size = 299
+batch_size = 100
+shuffle_buffer = 10000
+dataset = tf.data.TFRecordDataset(train_files)
+dataset = dataset.map(parser)#把数据用parser解析
+
+#开始shuffle和batching操作
+dataset = dataset.map(
+	lambda image, label: (
+		preprocess_for_train(image, image_size, image_size, None), label)
+	)
+dataset = dataset.shuffle(shuffle_buffer).batch(batch_size)
+
+NUM_EPOCH = 10
+dataset = dataset.repeat(NUM_EPOCH)#数据集重复次数
+iterator = dataset.make_initializer_iterator()
+image_batch, label_batch = iterator.get_next()#获取batch数据
+
+logit = inference(image_batch)
+loss =calc_loss(logit, label_batch)
+train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+
+test_dataset = tf.data.TFRecord(test_files)
+test_dataset = test_dataset.map(parser).map(
+	lambda label: ( 
+	tf.image.resize_images(image, [image_size, image_size], label))
+	)
+test_dataset = test_dataset.batch(batch_size)
+
+test_iterator = test_dataset.make_initializer_iterator()#定义测试数据上的迭代器
+test_image_batch, test_label_batch = test_iterator.get_next()
+
+test_logit = inference(test_image_batch)
+predictions = tf.argmax(test_logit, axis=-1, output_type=tf.int32)
+
+with tf.Session() as sess:
+	sess.run(tf.global_variables_initializer(),
+			tf.loacl_variables_initializer())
+
+	sess.run(iterator.initializer)
+	
+	while True:
+		try:
+			sess.run(train_step)
+		except tf.errors.OutOfRangeError:
+			break
+
+	sess.run(test_iterator.initializer)
+	test_results =[]
+	test_labels = []
+	while True:
+		try:
+			pred, label = sess.run([predictions, test_label_batch])
+			test_results.extend(pred)
+			test_labels.extend(label)
+		except tf.errors.OutOfRangeError:
+			break
+			
+correct = (float(y == y_) for (y, y_） in zip(test_results, test_labels)]
+accuracy= sum(correct) / len(correct)
+print (”Test accuracy is:”, accuracy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
